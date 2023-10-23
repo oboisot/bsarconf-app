56c977271444607813eefb80b540522137caccd6
oboisot <olivier.boisot@gmail.com>
2023-10-23 18:14:34 +0200

    update

--- src/js/scripts/initapp.js
+++ src/js/scripts/initapp.js
@@ -1,16 +1,17 @@
 import WebGL from '../three/capabilities/WebGL.js';
 
 if ( !WebGL.isWebGLAvailable() ) {
-    const warning = WebGL.getWebGLErrorMessage();
     const loader = document.getElementById( 'loader' );
+    const closeButton = document.getElementById( 'closeButton' );
+    const warning = WebGL.getWebGLErrorMessage();
+    // Loader update
     loader.style['background-image'] = "None";
     loader.style['text-align'] = "center";
     loader.style['top'] = loader.style['left'] = "0";
     loader.style.width = "100%";
     loader.style.height = "100%";
     loader.textContent = `${warning.textContent}`;
-    // loader.appendChild( warning );
-    const closeButton = document.getElementById( 'closeButton' );
+    // Close buttonupdate
     closeButton.style.display = "inline-block";
     closeButton.onclick = () => {
         window.__TAURI__.process.exit(1);
